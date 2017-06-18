aea07690890e885ef3ecd1338aec2152c152a0cd
Jia Min <jiaminw@ymail.com>
2017-06-18 17:27:19 +0800

    Added logout button

--- app/src/main/java/finapp/publicstatic/com/fintechbankapp/CardsActivity.java
+++ app/src/main/java/finapp/publicstatic/com/fintechbankapp/CardsActivity.java
@@ -1,5 +1,6 @@
 package finapp.publicstatic.com.fintechbankapp;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -49,18 +50,21 @@ public class CardsActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_layout, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will automatically handle clicks on the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/finapp/publicstatic/com/fintechbankapp/MainActivity.java
+++ app/src/main/java/finapp/publicstatic/com/fintechbankapp/MainActivity.java
@@ -1,5 +1,6 @@
 package finapp.publicstatic.com.fintechbankapp;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import layout.BankFragment;
@@ -66,6 +68,26 @@ public class MainActivity extends AppCompatActivity implements BankFragment.OnFr
                 });
 
         bottomNavigationView.setSelectedItemId(R.id.action_account);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onFragmentInteraction(Uri uri) {
