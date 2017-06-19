66360b1b673c23ebd4b875a74e690b684d131ced
Jia Min <jiaminw@ymail.com>
2017-06-20 02:48:28 +0800

    Updated Wallet Part - Incomplete

--- app/src/main/java/Adapter/CardsAdapter.java
+++ app/src/main/java/Adapter/CardsAdapter.java
@@ -1,10 +1,22 @@
 package Adapter;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,20 +25,29 @@ import Model.Cards;
 import finapp.publicstatic.com.fintechbankapp.R;
 
 public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> {
+
     private List<Cards> cardList = new ArrayList<>();
 
+    Context mContext;
+
     class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView cardId;
+        public TextView accountId;
         public TextView cardName;
         public TextView cardNum;
         public TextView cardExpiryDate;
         public TextView activate;
+        public Button btnScan;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            cardId = (TextView) itemView.findViewById(R.id.card_Id);
+            accountId = (TextView) itemView.findViewById(R.id.card_account_id);
             cardName = (TextView)itemView.findViewById(R.id.title_card_name);
             cardNum = (TextView)itemView.findViewById(R.id.title_card_num);
             cardExpiryDate = (TextView)itemView.findViewById(R.id.title_expiryDate);
             activate = (TextView)itemView.findViewById(R.id.title_activated);
+            btnScan = (Button)itemView.findViewById(R.id.button_scan);
         }
     }
 
@@ -37,6 +58,7 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View cardsView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_wallet_cardview, viewGroup, false);
+        mContext = viewGroup.getContext();
         ViewHolder viewHolder = new ViewHolder(cardsView);
         return viewHolder;
     }
@@ -44,6 +66,8 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         Cards cards = cardList.get(position);
+        viewHolder.cardId.setText(String.valueOf(cards.getCardsID()));
+        viewHolder.accountId.setText(String.valueOf(cards.getAccountID()));
         viewHolder.cardName.setText(String.valueOf(cards.getCardName()));
         viewHolder.cardNum.setText(String.valueOf(cards.getCardNum()));
         viewHolder.cardExpiryDate.setText(String.valueOf("Expiry Date: " + cards.getCardExpiryDate()));
@@ -58,4 +82,5 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
     public int getItemCount() {
         return cardList.size();
     }
+
 }
--- app/src/main/java/layout/WalletFragment.java
+++ app/src/main/java/layout/WalletFragment.java
@@ -1,24 +1,38 @@
 package layout;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v13.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.cooltechworks.creditcarddesign.CreditCardUtils;
+import com.google.zxing.client.android.CaptureActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,15 +44,22 @@ import java.util.List;
 
 import Adapter.CardsAdapter;
 import Model.Cards;
+import finapp.publicstatic.com.fintechbankapp.BillScheduleActivity;
+import finapp.publicstatic.com.fintechbankapp.CardRecyclerTouchListener;
 import finapp.publicstatic.com.fintechbankapp.JSONParser;
 import finapp.publicstatic.com.fintechbankapp.R;
+import finapp.publicstatic.com.fintechbankapp.TransSuccessfulActivity;
 import finapp.publicstatic.com.fintechbankapp.WebServiceAddress;
 import it.michelelacorte.swipeablecard.OptionView;
 import it.michelelacorte.swipeablecard.OptionViewAdditional;
 import it.michelelacorte.swipeablecard.SwipeableCard;
 import it.michelelacorte.swipeablecard.SwipeableCardAdapter;
 
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
+import static android.content.Context.CAMERA_SERVICE;
 
 public class WalletFragment extends Fragment {
 
@@ -47,6 +68,7 @@ public class WalletFragment extends Fragment {
     private CardsAdapter cardsAdapter;
     private CardsTask mCardsTask = null;
     private List<Cards> cardList = new ArrayList<>();
+    private static final int REQUEST_USE_CAMERA = 0;
 
     private OnFragmentInteractionListener mListener;
 
@@ -86,19 +108,40 @@ public class WalletFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_wallet, container, false);
 
+        mayRequestCamera();
+
         Button btnAddCard = (Button) v.findViewById(R.id.add_card_button);
-        btnAddCard.setOnClickListener(new View.OnClickListener()
-        {
+        btnAddCard.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(getActivity(),
                         com.cooltechworks.creditcarddesign.CardEditActivity.class);
-                startActivityForResult(intent,GET_NEW_CARD);
+                startActivityForResult(intent, GET_NEW_CARD);
             }
         });
 
         rv = (RecyclerView) v.findViewById(R.id.recycler_view_cards);
+        rv.addOnItemTouchListener(new CardRecyclerTouchListener
+                (getActivity().getApplicationContext(), rv, new
+                        CardRecyclerTouchListener.ClickListener() {
+                            public void onClick(View view, int position) {
+                                TextView cardId = (TextView) view.findViewById(R
+                                        .id.card_Id);
+                                TextView accountId = (TextView) view
+                                        .findViewById(R
+                                        .id.card_account_id);
+
+                                Intent intent = new Intent(getActivity(), CaptureActivity.class);
+                                intent.setAction("com.google.zxing.client.android.SCAN");
+                                intent.putExtra("SAVE_HISTORY", true);
+                                startActivityForResult(intent, 10001);
+                            }
+
+                            public void onLongClick(View view, int position) {
+                            }
+                        }));
+
+
         cardsAdapter = new CardsAdapter(cardList);
         mCardsTask = new CardsTask(mUserId);
         mCardsTask.execute((String) null);
@@ -109,23 +152,86 @@ public class WalletFragment extends Fragment {
         return v;
     }
 
+    private boolean mayRequestCamera() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (ContextCompat.checkSelfPermission(getActivity(), CAMERA) ==
+                PackageManager
+                        .PERMISSION_GRANTED) {
+            return true;
+        }
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(getActivity(), new
+                            String[]{CAMERA},
+                    REQUEST_USE_CAMERA);
+        }
+        return false;
+    }
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
     }
 
-    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent
+            intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
 
-        if(resultCode == RESULT_OK) {
+        Fragment fragment = getFragmentManager().findFragmentById(R.layout
+                .fragment_wallet);
+        if (fragment != null) {
+            fragment.onActivityResult(requestCode, resultCode, intent);
+        }
 
-            String cardHolderName = data.getStringExtra(CreditCardUtils.EXTRA_CARD_HOLDER_NAME);
-            String cardNumber = data.getStringExtra(CreditCardUtils.EXTRA_CARD_NUMBER);
-            String expiry = data.getStringExtra(CreditCardUtils.EXTRA_CARD_EXPIRY);
-            String cvv = data.getStringExtra(CreditCardUtils.EXTRA_CARD_CVV);
+        switch (resultCode) {
+            case RESULT_OK:
+                if (requestCode == GET_NEW_CARD) {
+                    String cardHolderName = intent.getStringExtra(CreditCardUtils.EXTRA_CARD_HOLDER_NAME);
+                    String cardNumber = intent.getStringExtra(CreditCardUtils.EXTRA_CARD_NUMBER);
+                    String expiry = intent.getStringExtra(CreditCardUtils.EXTRA_CARD_EXPIRY);
+                    String cvv = intent.getStringExtra(CreditCardUtils.EXTRA_CARD_CVV);
 
-            // Your processing goes here.
-            // addCard
+                    addCard(cardHolderName, cardNumber, expiry, cvv);
+
+                } else if (requestCode == 10001) {
+                    //get the extras that are returned from the intent
+                    String contents = intent.getStringExtra("SCAN_RESULT");
+
+                    MaterialDialog materialDialog = new MaterialDialog.Builder
+                            (getActivity())
+                            .title(R.string.string_dialog_title)
+                            .positiveText(R.string.input_positiveText)
+                            .negativeText(R.string.input_negativeText)
+                            .inputRangeRes(6, 6, R.color.colorPrimary)
+                            .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                            .input(null, null, new
+                                    MaterialDialog.InputCallback() {
+                                        @Override
+                                        public void onInput(MaterialDialog dialog, CharSequence input) {
+                                            // Do something
+                                        }
+                                    })
+                            .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                                }
+                            })
+                            .onNegative(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                                }
+                            }).show();
+
+                }
+                break;
+            case RESULT_CANCELED:
+                break;
         }
     }
 
@@ -186,7 +292,7 @@ public class WalletFragment extends Fragment {
             if (json != null) {
                 try {
                     success = json.getInt(TAG_SUCCESS);
-                    if(success == 1){
+                    if (success == 1) {
                         JSONArray jsonArray = json.getJSONArray("cards");
 
                         for (int i = 0; i < jsonArray.length(); i++) {
@@ -216,4 +322,112 @@ public class WalletFragment extends Fragment {
         protected void onCancelled() {
         }
     }
+
+    private void addCard(String cardHolderName, String  cardNumber, String
+            expiry, String  ccv){
+        JSONParser jsonParser = new JSONParser();
+        WebServiceAddress webServiceAddress = new WebServiceAddress();
+        String TAG_SUCCESS = "success";
+        int success = 0;
+
+        HashMap<String, String> params = new HashMap<>();
+
+        params.put("userId", mUserId);
+        params.put("accountId", mAcctId);
+        params.put("cardTypeId", mAmt);
+        params.put("cardNum", cardNumber);
+        params.put("ccv", ccv);
+        params.put("expiryDate", expiry);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        JSONObject json = jsonParser.makeHttpRequest(
+                "POST", webServiceAddress.getBaseUrl
+                        ("addTransaction"), params);
+
+        if (json != null) {
+            try {
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Intent intent = new Intent(getActivity(),
+                            TransSuccessfulActivity.class);
+                    intent.putExtra("amount", mAmt);
+                    intent.putExtra("payeeName", mPayeeName);
+
+                    if (mComment.equals("")) {
+                        intent.putExtra("comments", "");
+                    } else {
+                        intent.putExtra("comments", mComment);
+                    }
+                    startActivityForResult(intent, 10001);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void transferSubmit(){
+        String mAmt = amtEditText.getText().toString();
+        String mComment = commentEdittext.getText().toString();
+        int postionCategory = spinnerCategory.getSelectedItemPosition() + 1;
+
+        if(mAmt.equals("")){
+            Toast.makeText(getActivity(), R.string.error_empty_amount, Toast.LENGTH_LONG).show();
+        }  else if (Double.parseDouble(mAmt) > Double.parseDouble(mAcctAmount)){
+            Toast.makeText(getActivity(), R.string.error_amount, Toast.LENGTH_LONG).show();
+        } else {
+
+            double diffAmt = Double.parseDouble(mAcctAmount) - Double
+                    .parseDouble(mAmt);
+
+            JSONParser jsonParser = new JSONParser();
+            WebServiceAddress webServiceAddress = new WebServiceAddress();
+            String TAG_SUCCESS = "success";
+            int success = 0;
+
+            HashMap<String, String> params = new HashMap<>();
+
+            params.put("userId", mUserId);
+            params.put("accountId", mAcctId);
+            params.put("transactionAmount", mAmt);
+            params.put("transactionDetails", mComment);
+            params.put("payeeId", mPayeeId);
+            params.put("categoryId", String.valueOf(postionCategory));
+            params.put("amountLeft", String.valueOf(diffAmt));
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            JSONObject json = jsonParser.makeHttpRequest(
+                    "POST", webServiceAddress.getBaseUrl
+                            ("addTransaction"), params);
+
+            if (json != null) {
+                try {
+                    success = json.getInt(TAG_SUCCESS);
+                    if (success == 1) {
+                        Intent intent = new Intent(getActivity(),
+                                TransSuccessfulActivity.class);
+                        intent.putExtra("amount", mAmt);
+                        intent.putExtra("payeeName", mPayeeName);
+
+                        if (mComment.equals("")) {
+                            intent.putExtra("comments", "");
+                        } else {
+                            intent.putExtra("comments", mComment);
+                        }
+                        startActivityForResult(intent, 10001);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
